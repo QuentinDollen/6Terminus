f7416c555184a22e40f6f5f705f47aee678452c4
Pierre-Gilles <pglbmeto@gmail.com>
2022-11-11 17:10:31 +0100

    modification de la classe ferme

--- Model/ferme.py
+++ Model/ferme.py
@@ -3,4 +3,7 @@ import batiment as b
 
 class Ferme (b.Batiment):
     def __init__(self, posx, posy):
-        b.Batiment.__init__(self, , "", posx, posy, , , , , , )
+        b.Batiment.__init__(self,3, "Farm.png", posx, posy,4, 0,2,1,1,2,10,0,0)
+        crop_path = "crop.png"
+        harvet_path = "Weed.png"
+        ind_Harv = 0
