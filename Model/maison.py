e25589893ba38f14df66134a300356c4262dd67e
rtas-vadum <agrand630@gmail.com>
2022-12-05 11:48:57 +0100

    houses

--- Model/maison.py
+++ Model/maison.py
@@ -1,15 +1,124 @@
 import batiment as b
 
 
-#class Panneau():
+# class Panneau():
 class Maison_1(b.Batiment):
-    def __init__(self,x,y):
-        b.Batiment.__init__(self,1,11,x,y,)
+    def __init__(self, x, y):
+        b.Batiment.__init__(self, 1, 11, x, y, )
         self.name = 'Maison'
         self.nb_habitants = 0
         self.nourriture = []
         self.produits = []
         self.acces_eau = 0
         self.id = 10
+        self.des_prev = -99 # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
+        self.des_next = -10
+        # self.entNeeded = 0
+        self.watNeeded = 0
+        self.godNeeded = 0
+        # self.eduNeeded = 0
+        self.marketNeeded = 0
+        # self.barberNeeded = 0
+        # self.bathNeeded = 0
+        # self.medNeeded = 0
+        self.foodNeeded = 0
+        # self.potNeeded = 0
+        # self.oilNeeded = 0
+        # self.furNeeded = 0
+        # self.wineNeeded = 0
+        self.crimeIncrement = 3
+        self.crimeBase = 25
+        self.prospCap = 5
+        self.popLim = 5
+        self.taxMultiplier = 1
 
-#class Maison_2():
+
+class Maison_2():
+    def __init__(self, x, y):
+        b.Batiment.__init__(self, 1, 11, x, y, )
+        self.name = 'Maison'
+        self.nb_habitants = 0
+        self.nourriture = []
+        self.produits = []
+        self.acces_eau = 0
+        self.id = 11
+        self.des_prev = -12  # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
+        self.des_next = -5
+        # self.entNeeded = 0
+        self.watNeeded = 1
+        self.godNeeded = 0
+        # self.eduNeeded = 0
+        self.marketNeeded = 0
+        # self.barberNeeded = 0
+        # self.bathNeeded = 0
+        # self.medNeeded = 0
+        self.foodNeeded = 0
+        # self.potNeeded = 0
+        # self.oilNeeded = 0
+        # self.furNeeded = 0
+        # self.wineNeeded = 0
+        self.crimeIncrement = 3
+        self.crimeBase = 25
+        self.prospCap = 10
+        self.popLim = 7
+        self.taxMultiplier = 1
+
+class Maison_3():
+    def __init__(self, x, y):
+        b.Batiment.__init__(self, 1, 11, x, y, )
+        self.name = 'Maison'
+        self.nb_habitants = 0
+        self.nourriture = []
+        self.produits = []
+        self.acces_eau = 0
+        self.id = 12
+        self.des_prev = -7  # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
+        self.des_next = 0
+        # self.entNeeded = 0
+        self.watNeeded = 1
+        self.godNeeded = 0
+        # self.eduNeeded = 0
+        self.marketNeeded = 1
+        # self.barberNeeded = 0
+        # self.bathNeeded = 0
+        # self.medNeeded = 0
+        self.foodNeeded = 1
+        # self.potNeeded = 0
+        # self.oilNeeded = 0
+        # self.furNeeded = 0
+        # self.wineNeeded = 0
+        self.crimeIncrement = 3
+        self.crimeBase = 25
+        self.prospCap = 15
+        self.popLim = 9
+        self.taxMultiplier = 1
+
+class Maison_4():
+    def __init__(self, x, y):
+        b.Batiment.__init__(self, 1, 11, x, y, )
+        self.name = 'Maison'
+        self.nb_habitants = 0
+        self.nourriture = []
+        self.produits = []
+        self.acces_eau = 0
+        self.id = 11
+        self.des_prev = -2  # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
+        self.des_next = 4
+        # self.entNeeded = 0
+        self.watNeeded = 1
+        self.godNeeded = 1
+        # self.eduNeeded = 0
+        self.marketNeeded = 1
+        # self.barberNeeded = 0
+        # self.bathNeeded = 0
+        # self.medNeeded = 0
+        self.foodNeeded = 1
+        # self.potNeeded = 0
+        # self.oilNeeded = 0
+        # self.furNeeded = 0
+        # self.wineNeeded = 0
+        self.crimeIncrement = 3
+        self.crimeBase = 25
+        self.prospCap = 20
+        self.popLim = 11
+        self.taxMultiplier = 1
