8ce52e232ad2b1f5daa02e1d5e48daac3280adf1
rtas-vadum <74561195+rtas-vadum@users.noreply.github.com>
2022-12-07 15:44:08 +0100

    Update maison.py

--- Model/maison.py
+++ Model/maison.py
@@ -5,12 +5,11 @@ import batiment as b
 class Maison_1(b.Batiment):
     def __init__(self, x, y):
         b.Batiment.__init__(self, 1, 10, x, y, 10, -3, 1, 1, 3, 0)
-        self.name = 'Maison'
+        self.name = 'Maison 1'
         self.nb_habitants = 0
         self.nourriture = []
         self.produits = []
         self.acces_eau = 0
-        self.id = 10
         self.des_prev = -99  # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
         self.des_next = -10
         # self.entNeeded = 0
@@ -36,12 +35,11 @@ class Maison_1(b.Batiment):
 class Maison_2(b.Batiment):
     def __init__(self, x, y):
         b.Batiment.__init__(self, 1, 11, x, y, 0, -3, 1, 1, 3, 0)
-        self.name = 'Maison'
+        self.name = 'Maison 2'
         self.nb_habitants = 0
         self.nourriture = []
         self.produits = []
         self.acces_eau = 0
-        self.id = 11
         self.des_prev = -12  # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
         self.des_next = -5
         # self.entNeeded = 0
@@ -67,12 +65,11 @@ class Maison_2(b.Batiment):
 class Maison_3(b.Batiment):
     def __init__(self, x, y):
         b.Batiment.__init__(self, 1, 12, x, y, 0, -2, 1, 1, 2, 0)
-        self.name = 'Maison'
+        self.name = 'Maison 3'
         self.nb_habitants = 0
         self.nourriture = []
         self.produits = []
         self.acces_eau = 0
-        self.id = 12
         self.des_prev = -7  # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
         self.des_next = 0
         # self.entNeeded = 0
@@ -98,12 +95,11 @@ class Maison_3(b.Batiment):
 class Maison_4(b.Batiment):
     def __init__(self, x, y):
         b.Batiment.__init__(self, 1, 13, x, y, 0, -2, 1, 1, 2, 0)
-        self.name = 'Maison'
+        self.name = 'Maison 4'
         self.nb_habitants = 0
         self.nourriture = []
         self.produits = []
         self.acces_eau = 0
-        self.id = 11
         self.des_prev = -2  # cf https://gamefaqs.gamespot.com/pc/63635-caesar-iii/faqs/14466
         self.des_next = 4
         # self.entNeeded = 0
